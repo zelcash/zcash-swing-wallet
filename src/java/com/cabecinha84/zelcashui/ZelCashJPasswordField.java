b3799fa79f8b700812b7f00db77dd5cbecfe4a08
Cabecinha84 <valter.cabecinha@gmail.com>
2019-05-03 19:06:28 +0100

    Added clipboard options to password field.

--- src/java/com/cabecinha84/zelcashui/ZelCashJPasswordField.java
+++ src/java/com/cabecinha84/zelcashui/ZelCashJPasswordField.java
@@ -1,43 +1,93 @@
 package com.cabecinha84.zelcashui;
 
 import java.awt.Color;
+import java.awt.Toolkit;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.JPasswordField;
+import javax.swing.KeyStroke;
+import javax.swing.SwingUtilities;
+import javax.swing.text.DefaultEditorKit;
 import javax.swing.text.Document;
+
+import com.vaklinov.zcashui.LanguageUtil;
 
 public class ZelCashJPasswordField extends JPasswordField {
 	private Color backGroundColor = ZelCashUI.passwordfield;
 	private Color textColor = ZelCashUI.text;
+	private static LanguageUtil langUtil = LanguageUtil.instance();
 	public ZelCashJPasswordField() {
 		super();
 		this.setBackground(backGroundColor);
 		this.setForeground(textColor);
+		this.addClipBoardMenuOptions();
 	}
 
 	public ZelCashJPasswordField(Document doc, String txt, int columns) {
 		super(doc, txt, columns);
 		this.setBackground(backGroundColor);
 		this.setForeground(textColor);
+		this.addClipBoardMenuOptions();
 	}
 
 	public ZelCashJPasswordField(int columns) {
 		super(columns);
 		this.setBackground(backGroundColor);
 		this.setForeground(textColor);
+		this.addClipBoardMenuOptions();
 	}
 
 	public ZelCashJPasswordField(String text, int columns) {
 		super(text, columns);
 		this.setBackground(backGroundColor);
 		this.setForeground(textColor);
+		this.addClipBoardMenuOptions();
 	}
 
 	public ZelCashJPasswordField(String text) {
 		super(text);
 		this.setBackground(backGroundColor);
 		this.setForeground(textColor);
+		this.addClipBoardMenuOptions();
 	}
 
+	private void addClipBoardMenuOptions() {
+		this.addMouseListener(new MouseAdapter() {
+            public void mouseReleased(final MouseEvent e) {
+            	int accelaratorKeyMask = Toolkit.getDefaultToolkit ().getMenuShortcutKeyMask();
+            	if(SwingUtilities.isRightMouseButton(e)) {
+	                if (e.isPopupTrigger()) {
+	                    final ZelCashJTextField component = (ZelCashJTextField)e.getComponent();
+	                    final ZelCashJPopupMenu menu = new ZelCashJPopupMenu();
+	                    ZelCashJMenuItem item;
+	                    item = new ZelCashJMenuItem(new DefaultEditorKit.CopyAction());
+	                    item.setText(langUtil.getString("copy"));
+	                    item.setEnabled(component.getSelectionStart() != component.getSelectionEnd());
+	                    item.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C, accelaratorKeyMask));
+	                    menu.add(item);
+	                    item = new ZelCashJMenuItem(new DefaultEditorKit.CutAction());
+	                    item.setText(langUtil.getString("cut"));
+	                    item.setEnabled(component.isEditable() && component.getSelectionStart() != component.getSelectionEnd());
+	                    item.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_X, accelaratorKeyMask));
+	                    menu.add(item);
+	                    item = new ZelCashJMenuItem(new DefaultEditorKit.PasteAction());
+	                    item.setText(langUtil.getString("paste"));
+	                    item.setEnabled(component.isEditable());
+	                    item.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_V, accelaratorKeyMask));
+	                    menu.add(item);
+	                    menu.show(e.getComponent(), e.getX(), e.getY());
+	                }
+            	}
+            }
+        });
 		
+		this.getInputMap().put(KeyStroke.getKeyStroke(KeyEvent.VK_C, Toolkit.getDefaultToolkit().getMenuShortcutKeyMask()), DefaultEditorKit.copyAction);
+		this.getInputMap().put(KeyStroke.getKeyStroke(KeyEvent.VK_X, Toolkit.getDefaultToolkit().getMenuShortcutKeyMask()), DefaultEditorKit.cutAction);
+		this.getInputMap().put(KeyStroke.getKeyStroke(KeyEvent.VK_V, Toolkit.getDefaultToolkit().getMenuShortcutKeyMask()), DefaultEditorKit.pasteAction);
+		this.getInputMap().put(KeyStroke.getKeyStroke(KeyEvent.VK_A, Toolkit.getDefaultToolkit().getMenuShortcutKeyMask()), DefaultEditorKit.selectAllAction);
+		
+	}	
 }
 
